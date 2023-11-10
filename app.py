a7a6cccd70acc68418e1df314641957d66c399c8
jahnte <johncalvinkarume@gmail.com>
2023-11-10 15:37:48 -0800

    New Commit

--- app.py
+++ app.py
@@ -17,8 +17,9 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    db.create_all()  # Create the database tables
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()  # Create the database tables
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 # ... (previous code)
 
